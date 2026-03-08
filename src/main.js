3b2b2053f7a4940552433dd494020ee17d4528b9
Robert <robadobflobit@gmail.com>
2026-03-07 21:17:12 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// Robert Lai
+// Playable Postcard (Title WIP)
+// ~ Hours spent
+// A playable postcard intended for my mother. Search the scene of a beach of Mar Del Plata,
+// Argentina to find hidden letters that complete the unfinished message on the back of the 
+// postcard. 
+
+'use strict'
+
+let config = {
+    parent: 'myGame',
+    type: Phaser.AUTO,
+    height: 640,
+    width: 960,
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [ Load, Title, PostcardFront, PostcardBack, Credits]
+}
+let game = new Phaser.Game(config);
+let centerX = game.config.wdith/2
+let centerY = game.config.height/2
+let w = game.config.width
+let h = game.config.height
+const textSpacer = 64
+
+let cursors
